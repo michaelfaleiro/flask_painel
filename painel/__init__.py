359f47a697c8f81595921331ba0ffd1ec0c96c06
Michael <michaelfaleiro@gmail.com>
2023-05-25 17:22:19 -0300

    configurado env

--- painel/__init__.py
+++ painel/__init__.py
@@ -1,17 +1,22 @@
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://michael:xbX3XAEcWnJRroa6nt3ECXlfzWlFArJ0@dpg-chnbj11mbg5577kjht10-a.oregon-postgres.render.com/banco_cvu7'
-app.config["SECRET_KEY"] = "b7fb527958e00800faaea98f67a7813d"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
-from painel.home import home_routes
+
+
 from painel.auth import auth_routes
+from painel.home import home_routes
