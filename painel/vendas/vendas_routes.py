3553942ee5ee6d57cd15a3d56422020bc976aad9
Michael <michaelfaleiro@gmail.com>
2023-05-26 18:20:00 -0300

    add criação nova venda

--- painel/vendas/vendas_routes.py
+++ painel/vendas/vendas_routes.py
@@ -1,11 +1,31 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from flask_login import login_required
 from painel import app, db
 from painel.vendas.forms import FormNovaVenda
+from .models import Vendas
 
 
-@app.route('/novavenda')
+@app.route('/vendas')
+@login_required
+def homevendas():
+    vendas = Vendas.query.all()
+    return render_template('vendas/homevendas.html', card=vendas)
+
+
+@app.route('/novavenda', methods=['GET', 'POST'])
 @login_required
 def novavenda():
     form = FormNovaVenda()
-    return render_template('vendas/homevendas.html', form=form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            venda = Vendas(nome=form.nome.data,
+                           carro=form.carro.data,
+                           placa=form.placa.data,
+                           chassi=form.chassi.data
+                           )
+            db.session.add(venda)
+            db.session.commit()
+            print(venda)
+            return redirect('vendas')
+    vendas = Vendas.query.all()
+    return render_template('vendas/homevendas.html', form=form, card=vendas)
